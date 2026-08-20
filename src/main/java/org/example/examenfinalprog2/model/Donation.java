1e6b029064c6552af2071e7757b2ca4cccecaa3b
Nombana <ramnombs@gmail.com>
2026-08-20 11:14:15 +0300

    refactor: Using records in models

--- src/main/java/org/example/examenfinalprog2/model/Donation.java
+++ src/main/java/org/example/examenfinalprog2/model/Donation.java
@@ -1,23 +1,13 @@
 package org.example.examenfinalprog2.model;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.math.BigDecimal;
 import java.time.Instant;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@EqualsAndHashCode(callSuper = true)
-public class Donation extends CashFlow {
-
-    private String comment;
-
-    public Donation(String id, Instant createdAt, BigDecimal amount, String userId, String comment) {
-        super(id, createdAt, amount, userId);
-        this.comment = comment;
-    }
+public record Donation(
+        String id,
+        Instant createdAt,
+        BigDecimal amount,
+        String userId,
+        String comment
+) implements CashFlow {
 }
