b7497b023213541b218c34d332b1f4babf9757d1
Nombana <ramnombs@gmail.com>
2026-08-20 10:35:05 +0300

    Initial project structure

---
+++ src/main/java/org/example/examenfinalprog2/ExamenFinalProg2Application.java
@@ -0,0 +1,13 @@
+package org.example.examenfinalprog2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalProg2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenFinalProg2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/examenfinalprog2/ExamenFinalProg2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.examenfinalprog2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalProg2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
